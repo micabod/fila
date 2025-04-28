1c4f1a04f5dbf26d408076089b0b189f1fe0753a
micabod <167199490+micabod@users.noreply.github.com>
2025-04-28 17:22:12 -0300

    listo

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public string codigoEntrada;
+    public int cant;
+    int precio;
+    int precio_final;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        if(codigoEntrada!="b" && codigoEntrada!="n" && codigoEntrada != "p"){
+            Debug.Log("codigo invalido");
+            return;
+        }
+        if (codigoEntrada=="b" && cant>4 || codigoEntrada == "n" && cant>6){
+            Debug.Log("Ingresar menor cantidad");
+            return;
+        }
+        if (codigoEntrada=="b"){
+            codigoEntrada="basico";
+            precio=800;
+        }
+        if (codigoEntrada=="n"){
+            codigoEntrada="normal";
+            precio=1200;
+        }
+        if (codigoEntrada=="p"){
+            codigoEntrada="premium";
+            precio=2000;
+        }
+        precio_final=precio * cant;
+        Debug.Log("La entrada es "+ codigoEntrada);
+        Debug.Log("la cantidad es de"+ cant);
+        Debug.Log("el precio es de"+precio_final);
     }
 
     // Update is called once per frame
